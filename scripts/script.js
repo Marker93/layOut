88f2c1d0ad1e6580818eead987ac1fde77120e03
Mark Dyakov <Marcus-5@yandex.ru>
2019-12-14 03:09:25 +0300

    Adding counter numbers and webpack

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,22 @@
+import counterUp from 'counterup2';
+
+
+//Carousel-3d
 $("#carousel").Cloud9Carousel( {
     buttonLeft: $("#buttons > .left"),
     buttonRight: $("#buttons > .right"),
     autoPlay: 1,
     bringToFront: true,
     yOrigin: 120,
-  } );
+});
+
+
+// Counter 
+const el = document.querySelectorAll( '.counter-count' );
+
+el.forEach(item => counterUp( item, {
+  duration: 2000,
+  delay: 16,
+} ) )
+
+
